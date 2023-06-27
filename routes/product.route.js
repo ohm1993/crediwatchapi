7788d7565f07a091eec5138df63bf4c1ad14edb3
ohm1993 <ohm2393@gmail.com>
2023-06-27 11:52:36 +0530

    pagination added for products get api

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const paginate = require('jw-paginate');
 const app = express();
 const router = express.Router();
 // User schema
 let ProductSchema = require('../model/product.model');
 // Get products
 router.route('/').get((req, res) => {
-  ProductSchema.find().then((products) => {
-        res.json({status:true,code:200,data:products})
+  ProductSchema.find().then((data) => {
+         const page = parseInt(req.query.page) || 1;
+         const pageSize = 6;
+         const pager = paginate(data.length, page, pageSize);
+         const  products = data.slice(pager.startIndex, pager.endIndex + 1);
+         res.json({status:true,code:200,data:products,pager:pager})
   })
   .catch((err) => {
     res.json({status:false,code:500,message:err.message})
